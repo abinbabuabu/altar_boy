3ad5706966a3d09dfc0671d4641977b7919d098e
Abin Babu <abin.babu01@sap.com>
2021-10-02 12:29:39 +0530

    optimised imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:altar_boy/account_registration/account_registration_widget.dart';
-import 'package:altar_boy/home_page/home_page_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
